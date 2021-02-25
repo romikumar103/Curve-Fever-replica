9a5b5cd04588fa764f9a994f734c419acefe9d61
romikumar103 <romikumar103@users.noreply.github.com>
2021-02-25 13:14:42 +0530

    initial commit
    initial commit

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool hasEnded = false;
+   public void EndGame()
+    {
+        if (hasEnded)
+            return;
+        hasEnded = true;
+        StartCoroutine(PlayEndGameAnimation());
+        Debug.Log("Game Over!");
+    }
+    
+    IEnumerator PlayEndGameAnimation()
+    {
+        Debug.Log("Game Over");
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ Assets/Script/SnakeMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeMovement : MonoBehaviour
+{
+    public float s_speed = 3f;
+    public float rotationSpeed = 200f;
+    float horizontal = 0f;
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(Vector2.up * s_speed * Time.fixedDeltaTime,Space.Self);
+        transform.Rotate(Vector3.forward * -horizontal * rotationSpeed * Time.fixedDeltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "killsPlayer")
+        {
+            s_speed = 0f;
+            rotationSpeed = 0f;
+
+            GameObject.FindObjectOfType<GameManager>().EndGame();
+          
+        }
+    }
+}
